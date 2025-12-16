698a68dc2827dbf9f59ecddde4dad089e1694b79
Nurgul Amirkhan <100270939+nurgu1@users.noreply.github.com>
2025-12-16 16:14:42 +0100

    Refactor SQL queries for air quality data

--- athena.sql
+++ athena.sql
@@ -27,4 +27,20 @@ ORDER BY high_risk_days DESC
 LIMIT 10;
 
 
+SELECT
+  c.city,
+  CAST(c.date AS date) AS dt,
+  c."pm2.5" AS historical_pm25,
+  a.pm2_5   AS realtime_pm25,
+  c.pm10    AS historical_pm10,
+  a.pm10    AS realtime_pm10,
+  c.no2     AS historical_no2,
+  a.nitrogen_dioxide AS realtime_no2
+FROM csv_air_quality c
+LEFT JOIN api_air_quality_view a
+  ON CAST(c.date AS date) = a.dt
+WHERE c.city IN ('Dubai', 'Mumbai', 'Tokyo')
+ORDER BY CAST(c.date AS date) DESC
+LIMIT 20;
+
 
