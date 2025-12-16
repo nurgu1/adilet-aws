5fafa71580ed4e541001df89eb04a7c967156dbd
Nurgul Amirkhan <100270939+nurgu1@users.noreply.github.com>
2025-12-16 15:54:57 +0100

    Add additional queries to athena.sql

--- athena.sql
+++ athena.sql
@@ -6,5 +6,25 @@ GROUP BY city
 ORDER BY avg_pm25 DESC
 LIMIT 10;
 
+SELECT
+    timestamp,
+    pm2_5,
+    pm10,
+    ozone,
+    nitrogen_dioxide
+FROM "20251214"
+ORDER BY timestamp DESC
+LIMIT 10;
+
+
+SELECT
+    city,
+    COUNT(*) AS high_risk_days
+FROM csv_air_quality
+WHERE "pm2.5" > 35
+GROUP BY city
+ORDER BY high_risk_days DESC
+LIMIT 10;
+
 
 
